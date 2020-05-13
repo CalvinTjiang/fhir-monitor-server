51a2d176bf4e97815af94e7a262b06333238a542
Marcel Go <marceljonathango@gmail.com>
2020-05-13 13:56:07 +1000

    added export default to Observer

--- src/observers/Observer.ts
+++ src/observers/Observer.ts
@@ -1,7 +1,7 @@
 /**
  * An Abstract class Observer which are the base class of all Observer
  */
-interface Observer {
+export default interface Observer {
 
     /**
      * Update method that will be called by the subject to tell the Observer to update its data
