c95b5a07feff92055cb015a4792dfe24f4d8935a
Marcel Go <marceljonathango@gmail.com>
2020-06-17 17:12:42 +1000

    Added blood pressure code

--- src/models/StatCode.ts
+++ src/models/StatCode.ts
@@ -2,7 +2,8 @@
  * enumeration for categories of measurements
  */
 enum StatCode {
-    TOTAL_CHOLESTEROL = "2093-3"
+    TOTAL_CHOLESTEROL = "2093-3",
+    BLOOD_PRESSURE = "55284-4"
 }
 
 export default StatCode;
