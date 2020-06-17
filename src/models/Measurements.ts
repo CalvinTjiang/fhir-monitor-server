25a3bc3079547f933d5de5a9cf317939f0f9a1be
Marcel Go <marceljonathango@gmail.com>
2020-06-17 15:44:36 +1000

    Added comments and fixed condition

--- src/models/Measurements.ts
+++ src/models/Measurements.ts
@@ -5,8 +5,14 @@ export default class Measurements {
     private measurements: {[statCode: string]: Array<Measurement>} = {};
     private static MAX_DATA_COUNT_EACH_MEASUREMENT = 5;
 
+    /**
+     * Add measurement to this Measurements, make sure there are only 5 maximum data with the latest effectiveDateTime
+     * @param measurement the measurement that will be added to this measurements
+     * @return boolean true if the given measurement is new measurement and it is properly added to the array
+     */
     public addMeasurement(measurement: Measurement) : boolean{
         let statCode: StatCode = measurement.getStatCode();
+        // create empty array if the entry in dictionary is still undefined
         if (!this.measurements[statCode]) {
             this.measurements[statCode] = new Array<Measurement>();
         }
@@ -21,7 +27,7 @@ export default class Measurements {
         // insert measurement sorted by effectiveDateTime descending
         this.measurements[statCode].push(measurement);
         for(let i: number = this.measurements[statCode].length - 1; i >= 1; i--) {
-            if (this.measurements[statCode][i] > this.measurements[statCode][i-1]) {
+            if (this.measurements[statCode][i].getEffectiveDateTime() > this.measurements[statCode][i-1].getEffectiveDateTime()) {
                 let temp: Measurement = this.measurements[statCode][i];
                 this.measurements[statCode][i] = this.measurements[statCode][i-1];
                 this.measurements[statCode][i-1] = temp;
@@ -36,6 +42,11 @@ export default class Measurements {
         return updated;
     }
 
+    /**
+     * Get 5 of the latest measurements
+     * @param statcode the statcode of the measurement
+     * @return An array of measurements
+     */
     public getMeasurements(statcode:StatCode): Array<Measurement> | null {
         if (!this.measurements[statcode]) {
             return null;
@@ -43,6 +54,11 @@ export default class Measurements {
         return this.measurements[statcode];
     }
 
+    /**
+     * Return the measurement with the given statCode with the latest effectiveDateTime
+     * @param statCode the statcode of the measurement
+     * @return the latest measurement or null if there is no measurement
+     */
     public getLatestMeasurement(statCode: StatCode): Measurement | null {
         if (!this.measurements[statCode]) {
             return null;
