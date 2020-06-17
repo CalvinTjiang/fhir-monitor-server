80b5b9176e5330e640bad83aa75e3259966a7985
Marcel Go <marceljonathango@gmail.com>
2020-06-17 17:50:53 +1000

    Added diastolic and systolic code

--- src/models/StatCode.ts
+++ src/models/StatCode.ts
@@ -3,7 +3,9 @@
  */
 enum StatCode {
     TOTAL_CHOLESTEROL = "2093-3",
-    BLOOD_PRESSURE = "55284-4"
+    BLOOD_PRESSURE = "55284-4",
+    SYSTOLIC_BLOOD_PRESSURE = "8480-6",
+    DIASTOLIC_BLOOD_PRESSURE = "8462-4"
 }
 
 export default StatCode;
